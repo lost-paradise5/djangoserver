6233614cc38c6a6f9e6cbc672a217d7a49e712bd
lost-paradise5 <76122692+lost-paradise5@users.noreply.github.com>
2026-03-12 19:24:39 +0800

    Update urls.py

--- frostproject/urls.py
+++ frostproject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 from frostapp.views import (
     queue_create,
     queue_update,
